0d3ec5349fedd104673a3afc282bb8493f006199
Jaco Brewis <jacobrew@gmail.com>
2025-05-04 09:56:26 +0200

    circles now display country names

--- src/app/components/ui/d3-container/d3-container.component.ts
+++ src/app/components/ui/d3-container/d3-container.component.ts
@@ -26,90 +26,90 @@ type PackedNode = d3.HierarchyCircularNode<HierarchyDatum>;
   templateUrl: './d3-container.component.html',
   styleUrls: ['./d3-container.component.scss']
 })
-export class D3ContainerWorld implements OnInit, AfterViewInit, OnDestroy {
+export class D3ContainerWorld implements OnInit, OnDestroy {
 
   @ViewChild('svg', { static: true }) svgRef!: ElementRef<SVGElement>;
 
-  @Input() hierachy!: HierarchyDatum;
+  hierachy!: HierarchyDatum;
 
   viewModel$!: Observable<D3DataModel>
-  destroy$ =  new Subject<void>();
+  destroy$ = new Subject<void>();
 
-  constructor(private store: Store) {}
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
-      this.viewModel$ = this.store.select(D3Selectors.getViewModel);
+    this.viewModel$ = this.store.select(D3Selectors.getViewModel);
 
-      this.viewModel$
+    this.viewModel$
       .pipe(
         takeUntil(this.destroy$)
       )
       .subscribe(data => {
-        this.hierachy = data.hierachy
+        if (!data.hierachy) { return;}
+        this.drawData(data.hierachy);
       })
 
   }
 
-  ngAfterViewInit(): void {
-
+  private drawData(data: HierarchyDatum) {
     // map data 
-    console.log(this.hierachy);
 
+    if (data &&  data.children && data?.children?.length > 1) {
+      const svg = d3.select(this.svgRef.nativeElement);
+      const width = 600;
+      const height = 600;
   
-
-    const svg = d3.select(this.svgRef.nativeElement);
-    const width = 600;
-    const height = 600;
-
-    const root = d3
-      .hierarchy<HierarchyDatum>(this.hierachy)
-      .sum(d => d.value || 0)
-      .sort((a, b) => b.value! - a.value!);
-
+      const root = d3
+        .hierarchy<HierarchyDatum>(data)
+        .sum(d => d.value || 0)
+        .sort((a, b) => b.value! - a.value!);
+  
       console.log('Descendants:', root.descendants().map(d => ({
         name: d.data.name,
         value: d.value
       })));
+  
+  
+      const packedRoot = d3.pack<HierarchyDatum>()
+        .size([width, height])
+        .padding(5)(root);
+  
+      const nodes = svg
+        .selectAll('circle')
+        .data(packedRoot.descendants()) // Type: PackedNode[]
+        .enter()
+        .append('circle')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+        .attr('r', d => d.r)
+        .attr('fill', d => d.children ? '#69b3a2' : '#40a9f3')
+        .attr('stroke', '#fff');
+  
+      const nodeGroups = svg
+        .selectAll('g')
+        .data(packedRoot.descendants())
+        .enter()
+        .append('g')
+        .attr('transform', d => `translate(${d.x}, ${d.y})`);
+  
+      nodeGroups
+        .append('circle')
+        .attr('r', d => d.r)
+        .attr('fill', d => d.children ? '#69b3a2' : '#40a9f3')
+        .attr('stroke', '#fff');
+  
+      nodeGroups
+        .filter(d => !d.children) // only label leaf nodes (countries)
+        .append('text')
+        .text(d => d.data.name)
+        .attr('dy', '0.3em')
+        .style('text-anchor', 'middle')
+        .style('pointer-events', 'none') // so clicks go through the text
+        .style('fill', '#fff')
+        .style('font-size', d => `${Math.min(2 * d.r, (2 * d.r) / d.data.name.length)}px`);
+    }
 
-
-    const packedRoot = d3.pack<HierarchyDatum>()
-      .size([width, height])
-      .padding(5)(root);
-
-    const nodes = svg
-      .selectAll('circle')
-      .data(packedRoot.descendants()) // Type: PackedNode[]
-      .enter()
-      .append('circle')
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y)
-      .attr('r', d => d.r)
-      .attr('fill', d => d.children ? '#69b3a2' : '#40a9f3')
-      .attr('stroke', '#fff');
-
-    // const nodeGroups = svg
-    //   .selectAll('g')
-    //   .data(packedRoot.descendants())
-    //   .enter()
-    //   .append('g')
-    //   .attr('transform', d => `translate(${d.x}, ${d.y})`);
-
-    // nodeGroups
-    //   .append('circle')
-    //   .attr('r', d => d.r)
-    //   .attr('fill', d => d.children ? '#69b3a2' : '#40a9f3')
-    //   .attr('stroke', '#fff');
-
-    // nodeGroups
-    //   .filter(d => !d.children) // only label leaf nodes (countries)
-    //   .append('text')
-    //   .text(d => d.data.name)
-    //   .attr('dy', '0.3em')
-    //   .style('text-anchor', 'middle')
-    //   .style('pointer-events', 'none') // so clicks go through the text
-    //   .style('fill', '#fff')
-    //   .style('font-size', d => `${Math.min(2 * d.r, (2 * d.r) / d.data.name.length)}px`);
-
+   
   }
 
   ngOnDestroy(): void {
